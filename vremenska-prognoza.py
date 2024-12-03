e3d4d54f1be2f142933a8ac186ba1f589b74c1fb
Djordje Uskokovic <djordje.uskokovic@link.co.rs>
2024-12-03 20:03:47 +0100

    dodat test

--- vremenska-prognoza.py
+++ vremenska-prognoza.py
@@ -1,10 +1,14 @@
 temperatura = int(input("Unesite temperaturu: "))
-
+poruka = ""
 if temperatura < 0:
-    print("Oprez klizavo")
+    poruka = "Oprez klizavo"
 
 if temperatura > 0:
     print("Temperatura iznad 0")
     if temeperatura > 30:
-        print("Ukljucite klima uredjaj")
-        
+        poruka = "Ukljucite klima uredjaj"
+
+test_temperatura = -1
+ocekivana_poruka = "Oprez klizavo"
+if poruka == ocekivana_poruka
+print("Case - ispod nule - Test prosao")
